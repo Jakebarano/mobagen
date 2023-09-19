3b3a5fd774448c7c1a1e24d7c02423435fe79618
Jake Baranowski <jake.baranowski@mymail.champlain.edu>
2023-09-18 22:51:06 -0400

    glider no longer explodes but the behavior is off.

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,22 +3,21 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
 
-  for (int y = 0; y < world.SideSize() ; ++y)
+  for (int y = 0; y <= world.SideSize() ; ++y)
   {
-    for (int x = 0; x < world.SideSize() ; ++x)
+    for (int x = 0; x <= world.SideSize() ; ++x)
     {
       int neighbors = CountNeighbors(world, {x, y});
 
-      if ( world.Get({x, y}) && (neighbors <= 1 || neighbors > 3)) //checks if cell has too few/many neighbors and dies.
+      if ( world.Get({x, y}) && neighbors < 2 || neighbors > 3) //checks if cell has too few/many neighbors and dies.
       {
         world.SetNext({x, y}, false);
       }
-      else if (world.Get({x, y}) && (neighbors == 2 || neighbors == 3))  //checks if the cell is alive and has enough neighbors to live.
+      if (world.Get({x, y}) && neighbors == 2 || neighbors == 3)  //checks if the cell is alive and has enough neighbors to live.
       {
         world.SetNext({x, y}, true);
       }
-
-      else if (!world.Get({x, y}) && neighbors == 3) //checks if cell is dead but has enough neighbors to become alive.
+      if (world.Get({x, y}) && neighbors == 3) //checks if cell is dead but has enough neighbors to become alive.
       {
         world.SetNext({x, y}, true);
       }
@@ -33,7 +32,7 @@ int JohnConway::CountNeighbors(World& world, Point2D point) {
   {
     for (int x = point.x - 1; x <= point.x + 1 ; x++)
     {
-      if (x != 0 && y != 0 && world.Get({point.x + x, point.y + y}))
+      if (x != 0 && y != 0 && world.Get({x, y}))
       {
         cellNeighbors++;
       }
