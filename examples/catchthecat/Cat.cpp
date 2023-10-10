dfb1b406711a82f88afcfa3c35d28856d35c46ff
Jake Baranowski <jake.baranowski@mymail.champlain.edu>
2023-10-10 12:33:01 -0400

    A* is corrosive pt 2

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -1,10 +1,15 @@
 #include "Cat.h"
 #include "World.h"
 #include <stdexcept>
+#include <queue>
+#include <map>
+
+using namespace std;
 
 Point2D Cat::Move(World* world) {
-  auto rand = Random::Range(0, 5);
+  auto rand = Random::Range(0, 5); //change out for
   auto pos = world->getCat();
+
   switch (rand) {
     case 0:
       return World::NE(pos);
@@ -21,4 +26,18 @@ Point2D Cat::Move(World* world) {
     default:
       throw "random out of range";
   }
+
+queue<Point2D> frontier;
+
+frontier.push(world->getCat());
+map came_from = dict() # path A->B is stored as came_from[B] == A
+came_from[start] = None
+
+while not frontier.empty():
+ current = frontier.get()
+ for next in graph.neighbors(current):
+    if next not in came_from:
+       frontier.put(next)
+       came_from[next] = current
+
 }
