e2a342490954ff3d3e244d37ea1a5c3f8ef3f802
Jake Baranowski <jake.baranowski@mymail.champlain.edu>
2023-10-14 16:47:37 -0400

    removed commented old/unused code

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -40,42 +40,5 @@ Point2D Cat::Move(World* world) {
           rand--;
       }
     }
-
-//    auto rand = Random::Range(0, 5); //change out for
-//
-//    switch (rand) {
-//      case 0:
-//        if (!world->getContent(World::NE(pos))) {
-//          return World::NE(pos);
-//        }
-//        break;
-//      case 1:
-//        if (!world->getContent(World::NW(pos))) {
-//          return World::NW(pos);
-//        }
-//        break;
-//      case 2:
-//        if (!world->getContent(World::E(pos))) {
-//          return World::E(pos);
-//        }
-//        break;
-//      case 3:
-//        if (!world->getContent(World::W(pos))) {
-//          return World::W(pos);
-//        }
-//        break;
-//      case 4:
-//        if (!world->getContent(World::SW(pos))) {
-//          return World::SW(pos);
-//        }
-//        break;
-//      case 5:
-//        if (!world->getContent(World::SE(pos))){
-//          return World::SE(pos);
-//        }
-//        break;
-//      default:
-//        return World::NE(pos);
-//    }
   }
 }
